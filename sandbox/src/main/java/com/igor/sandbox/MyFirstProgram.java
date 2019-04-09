e55e70d40755fcfe1b7958ce7de7dd52e2ca971f
ig <po4talen@gmail.com>
2019-04-09 14:26:36 -0700

    Added several functions

--- sandbox/src/main/java/com/igor/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/com/igor/sandbox/MyFirstProgram.java
@@ -3,6 +3,26 @@ package com.igor.sandbox;
 public class MyFirstProgram {
 
  public static void main(String[] args){
-	System.out.println("Hello, world!");
+	hello("world");
+	hello("user");
+	hello("Igor");
+	double l = 5;
+     System.out.println("Площа квадрата зі стороною " + l + " = " + area(l));
+
+     double a = 4;
+     double b = 6;
+     System.out.println("Площа прямокутника зі сторонами " + a + " та " + b + " = " + area(a, b));
+ }
+
+ public static void hello(String somebody){
+    System.out.println("Hello " + somebody + "!");
+ }
+
+ public static double area (double len){
+    return len * len;
+ }
+
+ public static double area (double a, double b){
+     return a * b;
  }
 }
