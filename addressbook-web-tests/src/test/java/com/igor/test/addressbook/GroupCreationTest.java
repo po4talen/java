015c617b0096fa09acd5a9bdbf57fe373cd1ab85
ig <po4talen@gmail.com>
2019-04-12 16:00:16 -0700

    Update Lesson 2 Refactoring

--- addressbook-web-tests/src/test/java/com/igor/test/addressbook/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/com/igor/test/addressbook/GroupCreationTest.java
@@ -3,94 +3,115 @@ package com.igor.test.addressbook;
 
 import java.util.concurrent.TimeUnit;
 
-import org.junit.Before;
 import org.testng.annotations.*;
+
 import static org.testng.Assert.*;
+
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 
-  public class GroupCreationTest {
-    private WebDriver driver;
-    private String baseUrl;
-    private boolean acceptNextAlert = true;
-    private StringBuffer verificationErrors = new StringBuffer();
+public class GroupCreationTest {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
 
-    @BeforeClass(alwaysRun = true)
-    public void setUp() throws Exception {
+  @BeforeClass(alwaysRun = true)
+  public void setUp() throws Exception {
 //firefox driver      System.setProperty("webdriver.gecko.driver","C:\\JAVA Trainings\\Barancev\\geckodriver-v0.24.0-win64\\geckodriver.exe");
-      System.setProperty("webdriver.gecko.driver","C:\\JAVA Trainings\\Barancev\\geckodriver-v0.24.0-win64\\chromedriver.exe");
-      driver = new FirefoxDriver();
-      baseUrl = "https://www.katalon.com/";
-      driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-      driver.get("http://localhost/addressbook/group.php");
-      driver.findElement(By.name("user")).clear();
-      driver.findElement(By.name("user")).sendKeys("admin");
-      driver.findElement(By.name("pass")).click();
-      driver.findElement(By.name("pass")).clear();
-      driver.findElement(By.name("pass")).sendKeys("secret");
-      driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]")).click();
-      driver.findElement(By.linkText("groups")).click();
-    }
+    System.setProperty("webdriver.gecko.driver", "C:\\JAVA Trainings\\Barancev\\geckodriver-v0.24.0-win64\\chromedriver.exe");
+    driver = new FirefoxDriver();
+    baseUrl = "https://www.katalon.com/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    driver.get("http://localhost/addressbook/group.php");
+    login("admin", "secret");
+  }
 
-    @Test
-    public void testGroupCreation() throws Exception {
+  private void login(String username, String password) {
+    driver.findElement(By.name("user")).clear();
+    driver.findElement(By.name("user")).sendKeys(username);
+    driver.findElement(By.name("pass")).click();
+    driver.findElement(By.name("pass")).clear();
+    driver.findElement(By.name("pass")).sendKeys(password);
+    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]")).click();
+     }
 
-      driver.findElement(By.name("new")).click();
-      driver.findElement(By.name("group_name")).click();
-      driver.findElement(By.name("group_name")).clear();
-      driver.findElement(By.name("group_name")).sendKeys("test1");
-      driver.findElement(By.name("group_header")).click();
-      driver.findElement(By.name("group_header")).clear();
-      driver.findElement(By.name("group_header")).sendKeys("test2");
-      driver.findElement(By.name("group_footer")).click();
-      driver.findElement(By.name("group_footer")).clear();
-      driver.findElement(By.name("group_footer")).sendKeys("test3");
-      driver.findElement(By.name("submit")).click();
-      driver.findElement(By.linkText("group page")).click();
-    }
+  @Test
+  public void testGroupCreation() throws Exception {
+    goToGroupPage("groups");
+    initGroupCreation();
+    fillGroupForm(new DataGroup("test1", "test2", "test3"));
+    submitGroupCreation();
+    goToGroupPage("group page");
+  }
 
-    @AfterClass(alwaysRun = true)
-    public void tearDown() throws Exception {
-      driver.quit();
-      String verificationErrorString = verificationErrors.toString();
-      if (!"".equals(verificationErrorString)) {
-        fail(verificationErrorString);
-      }
-    }
 
-    private boolean isElementPresent(By by) {
-      try {
-        driver.findElement(by);
-        return true;
-      } catch (NoSuchElementException e) {
-        return false;
-      }
-    }
+  private void submitGroupCreation() {
+    driver.findElement(By.name("submit")).click();
+  }
 
-    private boolean isAlertPresent() {
-      try {
-        driver.switchTo().alert();
-        return true;
-      } catch (NoAlertPresentException e) {
-        return false;
-      }
-    }
+  private void fillGroupForm(DataGroup dataGroup) {
+    driver.findElement(By.name("group_name")).click();
+    driver.findElement(By.name("group_name")).clear();
+    driver.findElement(By.name("group_name")).sendKeys(dataGroup.getName());
+    driver.findElement(By.name("group_header")).click();
+    driver.findElement(By.name("group_header")).clear();
+    driver.findElement(By.name("group_header")).sendKeys(dataGroup.getHeader());
+    driver.findElement(By.name("group_footer")).click();
+    driver.findElement(By.name("group_footer")).clear();
+    driver.findElement(By.name("group_footer")).sendKeys(dataGroup.getFooter());
+  }
 
-    private String closeAlertAndGetItsText() {
-      try {
-        Alert alert = driver.switchTo().alert();
-        String alertText = alert.getText();
-        if (acceptNextAlert) {
-          alert.accept();
-        } else {
-          alert.dismiss();
-        }
-        return alertText;
-      } finally {
-        acceptNextAlert = true;
-      }
+  private void initGroupCreation() {
+    driver.findElement(By.name("new")).click();
+  }
+
+  private void goToGroupPage(String groups) {
+    driver.findElement(By.linkText(groups)).click();
+  }
+
+  @AfterClass(alwaysRun = true)
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
     }
   }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
 
 
